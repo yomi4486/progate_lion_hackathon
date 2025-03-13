66897b1e3d2c2ee19f29455b398b3abca92a06f0
yomi4486 <yomi4486@users.noreply.github.com>
2025-03-13 05:58:19 +0000

    Apply Prettier Change

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,27 +1,27 @@
 // eslint.config.js
-const eslint = require('@eslint/js')
-const tseslint = require('typescript-eslint')
-const prettier = require('eslint-config-prettier')
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
 
 const eslintConfig = tseslint.config(
-	eslint.configs.recommended,
-	...tseslint.configs.recommended,
-	{
-		files: ['**/*.ts', '**/*.tsx'],
-		rules: {
-			'@typescript-eslint/explicit-function-return-type': 'off',
-			'@typescript-eslint/no-explicit-any': 'warn',
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{ argsIgnorePattern: '^_' },
-			],
-			'no-console': ['warn', { allow: ['warn', 'error'] }],
-		},
-	},
-	{
-		ignores: ['dist/', 'node_modules/', 'test/', 'eslint.config.cjs'],
-	},
-	prettier,
-)
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", "test/", "eslint.config.cjs"],
+  },
+  prettier,
+);
 
-module.exports = eslintConfig
+module.exports = eslintConfig;
