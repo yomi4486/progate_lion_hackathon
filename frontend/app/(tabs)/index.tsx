6dfb6f0750ffb478364ea77cd66ecbccf87f88ae
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-03-14 00:10:57 +0900

    Delete: console.log

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,22 +1,22 @@
-import { StyleSheet, Button } from "react-native";
-import { useRouter } from "expo-router";
-import { Authenticator } from "@aws-amplify/ui-react-native";
-import { Amplify } from "aws-amplify";
-import awsconfig from "../../src/aws-exports";
+import { StyleSheet, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Authenticator } from '@aws-amplify/ui-react-native';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../../src/aws-exports';
 
 Amplify.configure(awsconfig);
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   const router = useRouter();
 
   const handleLogin = async () => {
     try {
-      router.push("/home"); // Replace '/AnotherScreen' with your target screen path
+      router.push('/home'); // Replace '/AnotherScreen' with your target screen path
     } catch (error) {
-      console.log("Error signing in", error);
+      console.info('Error signing in', error);
     }
   };
 
@@ -34,16 +34,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%',
   },
 });
