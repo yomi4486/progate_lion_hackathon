18ba66dfb226b17f8cbda1d4c8af73ef82417acf
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-03-13 22:58:06 +0900

    Init: Amplify

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Auth } from 'aws-amplify';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      await Auth.signIn('username', 'password');
+      router.push('/home'); // Replace '/AnotherScreen' with your target screen path
+    } catch (error) {
+      console.log('Error signing in', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 }
