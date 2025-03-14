29870fcf2768d17ef803f126e949c6a66930b8f4
yomi4486 <yomi4486@users.noreply.github.com>
2025-03-14 17:05:53 +0000

    Apply Prettier Change

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,12 +1,16 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
 
 import { Authenticator } from "@aws-amplify/ui-react-native";
 import { Amplify } from "aws-amplify";
@@ -17,11 +21,11 @@ Amplify.configure(awsconfig);
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -29,7 +33,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -57,14 +61,18 @@ function RootLayoutNav() {
   return (
     <Authenticator.Provider>
       <Authenticator>
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Stack screenOptions={{headerShown: false }}>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="modal" options={{ presentation: 'modal', headerShown: false }} />
+            <Stack.Screen
+              name="modal"
+              options={{ presentation: "modal", headerShown: false }}
+            />
           </Stack>
         </ThemeProvider>
       </Authenticator>
     </Authenticator.Provider>
-
   );
 }
--- frontend/app/settings.tsx
+++ frontend/app/settings.tsx
@@ -11,20 +11,50 @@ import SimpleModal from "./components/simpleModal";
 import { useState } from "react";
 
 export default function TabOneScreen() {
-  const [isModalVisible, setModalVisible] = useState(false)
+  const [isModalVisible, setModalVisible] = useState(false);
   return (
     <View style={{ height: "100%" }}>
-      <SimpleModal visible={isModalVisible} visibleControler={()=>{setModalVisible(false)}}>
-        <View style={{alignItems: 'center',justifyContent: 'center',}}>
-        <Text style={{paddingBottom:10,fontSize:20,textAlign:"center"}}>本当にログアウトしますか？</Text>
-        <TouchableOpacity style={{padding: 10,width:"80%",backgroundColor:"#FF555544",borderRadius:10}} onPress={() => signOut()}>
-          <Text style={styles.title}>OK</Text>
-        </TouchableOpacity>
+      <SimpleModal
+        visible={isModalVisible}
+        visibleControler={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text
+            style={{ paddingBottom: 10, fontSize: 20, textAlign: "center" }}
+          >
+            本当にログアウトしますか？
+          </Text>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              width: "80%",
+              backgroundColor: "#FF555544",
+              borderRadius: 10,
+            }}
+            onPress={() => signOut()}
+          >
+            <Text style={styles.title}>OK</Text>
+          </TouchableOpacity>
         </View>
       </SimpleModal>
-      <DefaultHeader title="設定" showSettingButton={false} showBackButton={true} />
+      <DefaultHeader
+        title="設定"
+        showSettingButton={false}
+        showBackButton={true}
+      />
       <View style={styles.container}>
-        <TouchableOpacity style={{borderColor: 'black', padding: 10,width:"80%",backgroundColor:"#FF555544",borderRadius:10}} onPress={() => setModalVisible(true)}>
+        <TouchableOpacity
+          style={{
+            borderColor: "black",
+            padding: 10,
+            width: "80%",
+            backgroundColor: "#FF555544",
+            borderRadius: 10,
+          }}
+          onPress={() => setModalVisible(true)}
+        >
           <Text style={styles.title}>ログアウト</Text>
         </TouchableOpacity>
       </View>
@@ -42,7 +72,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     textAlign: "center",
-    color:"#FF5555cc"
+    color: "#FF5555cc",
   },
   separator: {
     marginVertical: 30,
