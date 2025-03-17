ab4df7fab669dc2c70c34e0913c3d1924fc357c7
WTNB1225 <WTNB1225@users.noreply.github.com>
2025-03-17 10:44:02 +0000

    Apply Prettier Change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,23 +9,22 @@ const verifier = CognitoJwtVerifier.create({
   userPoolId: process.env.USERPOOL_ID as string,
   tokenUse: "access",
   clientId: process.env.CLIENT_ID as string,
-})
+});
 
-const app = new Hono()
-  .use('*', async(c, next) => {
-    try{
-      const authHeader = c.req.header('Authorization')
-      if (!authHeader) {
-        return c.json({message: "Unauthorized"}, 401)
-      }
-      const token = authHeader.split(' ')[1]
-      const payload = await verifier.verify(token)
-      await next()
-      console.log(payload)
-    } catch(e) {
-        return c.json({ message: "Unauthorized" }, 401)
+const app = new Hono().use("*", async (c, next) => {
+  try {
+    const authHeader = c.req.header("Authorization");
+    if (!authHeader) {
+      return c.json({ message: "Unauthorized" }, 401);
     }
-  })
+    const token = authHeader.split(" ")[1];
+    const payload = await verifier.verify(token);
+    await next();
+    console.log(payload);
+  } catch (e) {
+    return c.json({ message: "Unauthorized" }, 401);
+  }
+});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
