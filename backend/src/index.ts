72e8030826e6927afb12c76ebf30f161bc057d31
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2025-03-17 19:29:41 +0900

    feature/wtnb1225/AddJwtVerify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,26 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
 
-const app = new Hono();
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: process.env.USER_POOL_ID as string,
+  tokenUse: "access",
+  clientId: process.env.CLIENT_ID as string,
+})
+
+const app = new Hono()
+  .use('*', async(c, next) => {
+    try{
+      const token = c.req.header('Authorization')
+      if (!token) {
+        return c.json({message: "Token is required"}, 401)
+      }
+      const payload = await verifier.verify(token)
+      console.log(payload)
+    } catch(e) {
+        return c.json({ message: "Unauthorized" }, 401)
+    }
+  })
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
