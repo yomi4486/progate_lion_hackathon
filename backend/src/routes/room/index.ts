c893d3d34e7caf2308337bf3ed965bce286a09cb
WTNB1225 <WTNB1225@users.noreply.github.com>
2025-03-20 13:09:16 +0000

    Apply Prettier Change

--- backend/src/routes/room/index.ts
+++ backend/src/routes/room/index.ts
@@ -10,90 +10,87 @@ config();
 
 const livekitHost = "https://progatehackathon-0vilmkur.livekit.cloud";
 const roomService = new RoomServiceClient(
-    livekitHost,
-    process.env.LIVEKIT_API_KEY,
-    process.env.LIVEKIT_API_SECRET,
+  livekitHost,
+  process.env.LIVEKIT_API_KEY,
+  process.env.LIVEKIT_API_SECRET,
 );
 const prisma = new PrismaClient();
 
 export const RoomRoute = new Hono<{ Variables: { userId: string } }>()
-    .get("/:id",
-        async (c) => {
-            const roomId = c.req.param("id");
-            const userId = c.get("userId");
+  .get("/:id", async (c) => {
+    const roomId = c.req.param("id");
+    const userId = c.get("userId");
 
-            const result = await prisma.room.findUnique({
-                where: {
-                    room_id: roomId,
-                },
-            });
-
-            if (!result) {
-                return c.json({ message: "Room not found" }, 404);
-            }
-
-            const at = new AccessToken(
-                process.env.LIVEKIT_API_KEY,
-                process.env.LIVEKIT_API_SECRET,
-                {
-                    identity: userId,
-                    ttl: "10m",
-                },
-            )
-            at.addGrant({ roomJoin: true, room: roomId });
-
-            return c.json({
-                token: at.toJwt(),
-                roomId: roomId,
-                room_owner_id: result.room_owner_id
-            })
-
-        }
-    )
-    .post("/", async (c) => {
-        const userId = c.get("userId");
-        const id = uuid();
-
-        const result = await prisma.room.create({
-            data: {
-                room_id: id,
-                room_owner_id: userId,
-            },
-        });
-
-        if (!result) {
-            return c.json({ message: "Failed to create room" }, 500);
-        }
-
-        const room = await roomService.createRoom({
-            name: id,
-            emptyTimeout: 10 * 60, // 10 minutes
-            maxParticipants: 100,
-        });
-        return c.json(room);
-    })
-    .delete("/:id", async (c) => {
-        const roomId = c.req.param("id");
-        const userId = c.get("userId");
-
-        const result = await prisma.room.findUnique({
-            where: {
-                room_id: roomId,
-                room_owner_id: userId,
-            },
-        });
-
-        if (!result) {
-            return c.json({ message: "Room not found" }, 404);
-        }
-
-        await prisma.room.delete({
-            where: {
-                room_id: roomId,
-                room_owner_id: userId,
-            },
-        });
-
-        await roomService.deleteRoom(roomId);
-        return c.json({ message: "Room deleted" });
+    const result = await prisma.room.findUnique({
+      where: {
+        room_id: roomId,
+      },
     });
+
+    if (!result) {
+      return c.json({ message: "Room not found" }, 404);
+    }
+
+    const at = new AccessToken(
+      process.env.LIVEKIT_API_KEY,
+      process.env.LIVEKIT_API_SECRET,
+      {
+        identity: userId,
+        ttl: "10m",
+      },
+    );
+    at.addGrant({ roomJoin: true, room: roomId });
+
+    return c.json({
+      token: at.toJwt(),
+      roomId: roomId,
+      room_owner_id: result.room_owner_id,
+    });
+  })
+  .post("/", async (c) => {
+    const userId = c.get("userId");
+    const id = uuid();
+
+    const result = await prisma.room.create({
+      data: {
+        room_id: id,
+        room_owner_id: userId,
+      },
+    });
+
+    if (!result) {
+      return c.json({ message: "Failed to create room" }, 500);
+    }
+
+    const room = await roomService.createRoom({
+      name: id,
+      emptyTimeout: 10 * 60, // 10 minutes
+      maxParticipants: 100,
+    });
+    return c.json(room);
+  })
+  .delete("/:id", async (c) => {
+    const roomId = c.req.param("id");
+    const userId = c.get("userId");
+
+    const result = await prisma.room.findUnique({
+      where: {
+        room_id: roomId,
+        room_owner_id: userId,
+      },
+    });
+
+    if (!result) {
+      return c.json({ message: "Room not found" }, 404);
+    }
+
+    await prisma.room.delete({
+      where: {
+        room_id: roomId,
+        room_owner_id: userId,
+      },
+    });
+
+    await roomService.deleteRoom(roomId);
+    return c.json({ message: "Room deleted" });
+  });
