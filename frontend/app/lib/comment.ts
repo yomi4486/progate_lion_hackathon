9c06262f5d8abb8bb0be89a4cc2a5a5410f1d1d7
WTNB1225 <WTNB1225@users.noreply.github.com>
2025-03-21 08:58:44 +0000

    Apply Prettier Change

--- frontend/app/lib/comment.ts
+++ frontend/app/lib/comment.ts
@@ -9,100 +9,106 @@ const updateFromRoomIdAndCommentId = client.comments[":roomId"][":commentId"];
 const deleteFromRoomIdAndCommentId = client.comments[":roomId"][":commentId"];
 
 export async function getComment(
-    idToken: string,
-    roomId: string
+  idToken: string,
+  roomId: string,
 ): Promise<InferResponseType<typeof getFromRoomId.$get, 200> | null> {
-    try {
-        const res = await client.comments[":roomId"].$get(
-            {
-                param: { roomId: roomId },
-            },
-            { headers: { Authorization: `Bearer ${idToken}` } },
-        );
-        if (res.status === 401) return null;
-        if (!res.ok) throw new Error("Failed request");
-        const jsonContent = await res.json();
-        return jsonContent;
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  try {
+    const res = await client.comments[":roomId"].$get(
+      {
+        param: { roomId: roomId },
+      },
+      { headers: { Authorization: `Bearer ${idToken}` } },
+    );
+    if (res.status === 401) return null;
+    if (!res.ok) throw new Error("Failed request");
+    const jsonContent = await res.json();
+    return jsonContent;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
 
 export async function createCommnet(
-    idToken: string,
-    comment: string,
-    videoPosition: number,
-    roomId: string
+  idToken: string,
+  comment: string,
+  videoPosition: number,
+  roomId: string,
 ): Promise<InferResponseType<typeof postFromRoomId.$post, 200> | null> {
-    try {
-        const res = await client.comments[":roomId"].$post(
-            {
-                json: {
-                    comment: comment,
-                    video_position: videoPosition
-                },
-                param: { roomId: roomId },
-            },
-            {
-                headers: { Authorization: `Bearer ${idToken}`}
-            }
-        );
-        if (!res.ok) throw new Error("Failed request");
-        const jsonContent = await res.json();
-        return jsonContent;
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  try {
+    const res = await client.comments[":roomId"].$post(
+      {
+        json: {
+          comment: comment,
+          video_position: videoPosition,
+        },
+        param: { roomId: roomId },
+      },
+      {
+        headers: { Authorization: `Bearer ${idToken}` },
+      },
+    );
+    if (!res.ok) throw new Error("Failed request");
+    const jsonContent = await res.json();
+    return jsonContent;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
 
 export async function updateComment(
-    idToken: string,
-    roomId: string,
-    commentId: string,
-    comment: string,
-): Promise<InferResponseType<typeof updateFromRoomIdAndCommentId.$put, 200> | null> {
-    try {
-        const res = await client.comments[":roomId"][":commentId"].$put(
-            {
-                json: {
-                    comment: comment
-                },
-                param: { roomId: roomId, commentId: commentId }
-            },
-            {
-                headers: { Authorization: `Bearer ${idToken}` }
-            }
-        )
-        if (!res.ok) throw new Error("Failed request");
-        const jsonContent = await res.json();
-        return jsonContent;
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  idToken: string,
+  roomId: string,
+  commentId: string,
+  comment: string,
+): Promise<InferResponseType<
+  typeof updateFromRoomIdAndCommentId.$put,
+  200
+> | null> {
+  try {
+    const res = await client.comments[":roomId"][":commentId"].$put(
+      {
+        json: {
+          comment: comment,
+        },
+        param: { roomId: roomId, commentId: commentId },
+      },
+      {
+        headers: { Authorization: `Bearer ${idToken}` },
+      },
+    );
+    if (!res.ok) throw new Error("Failed request");
+    const jsonContent = await res.json();
+    return jsonContent;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
 
 export async function deleteComment(
-    idToken: string,
-    roomId: string,
-    commentId: string,
-): Promise<InferResponseType<typeof deleteFromRoomIdAndCommentId.$delete, 200> | null> {
-    try {
-        const res = await client.comments[":roomId"][":commentId"].$delete(
-            {
-                param: { roomId: roomId, commentId: commentId }
-            },
-            {
-                headers: { Authorization: `Bearer ${idToken}` }
-            }
-        )
-        if (!res.ok) throw new Error("Failed request");
-        const jsonContent = await res.json();
-        return jsonContent;
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  idToken: string,
+  roomId: string,
+  commentId: string,
+): Promise<InferResponseType<
+  typeof deleteFromRoomIdAndCommentId.$delete,
+  200
+> | null> {
+  try {
+    const res = await client.comments[":roomId"][":commentId"].$delete(
+      {
+        param: { roomId: roomId, commentId: commentId },
+      },
+      {
+        headers: { Authorization: `Bearer ${idToken}` },
+      },
+    );
+    if (!res.ok) throw new Error("Failed request");
+    const jsonContent = await res.json();
+    return jsonContent;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
